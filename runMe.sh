84b37595f692e8b95871ce07d2cb1bfba1a9290a
kumbasar <vkumbasar@outlook.com>
2017-01-23 22:24:21 +0300

    add bash script and update readme

--- runMe.sh
+++ runMe.sh
@@ -1,7 +1,2 @@
-# aksgrt
-Installation:
-ghost:aksgrt kumbasar$ npm install
-
-Execution:
+#!/bin/bash
 node node_modules/cucumber/bin/cucumber.js features/mainPage.feature -r features/step_definitions/ -r features/support/hooks.js -r features/support/env.js -r features/support/world.js -f json:test/report/results.json
-
